50c6fda26c1e23306d4837f7e497499668ca5432
Andy <349562769@gapps.yrdsb.ca>
2024-07-24 17:05:41 -0400

    init starter files

---
+++ tetris.asm
@@ -0,0 +1,76 @@
+#####################################################################
+# CSCB58 Summer 2024 Assembly Final Project - UTSC
+# Student1: Name, Student Number, UTorID, official email
+# Student2: Name, Student Number, UTorID, official email
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 4 (update this as needed) 
+# - Unit height in pixels: 4 (update this as needed)
+# - Display width in pixels: 256 (update this as needed)
+# - Display height in pixels: 256 (update this as needed)
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4/5 (choose the one the applies)
+#
+# Which approved features have been implemented?
+# (See the assignment handout for the list of features)
+# Easy Features:
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# ... (add more if necessary)
+# Hard Features:
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# ... (add more if necessary)
+# How to play:
+# (Include any instructions)
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+
+##############################################################################
+
+    .data
+##############################################################################
+# Immutable Data
+##############################################################################
+# The address of the bitmap display. Don't forget to connect it!
+ADDR_DSPL:
+    .word 0x10008000
+# The address of the keyboard. Don't forget to connect it!
+ADDR_KBRD:
+    .word 0xffff0000
+
+##############################################################################
+# Mutable Data
+##############################################################################
+
+##############################################################################
+# Code
+##############################################################################
+	.text
+	.globl main
+
+	# Run the Tetris game.
+main:
+    # Initialize the game
+
+game_loop:
+	# 1a. Check if key has been pressed
+    # 1b. Check which key has been pressed
+    # 2a. Check for collisions
+	# 2b. Update locations (paddle, ball)
+	# 3. Draw the screen
+	# 4. Sleep
+
+    #5. Go back to 1
+    b game_loop
