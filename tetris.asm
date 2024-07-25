4ecb4b8608dcbf55d56761e3fde25ef97a616228
Andy <349562769@gapps.yrdsb.ca>
2024-07-24 20:31:42 -0400

    first two parts of milestone 1

--- tetris.asm
+++ tetris.asm
@@ -57,6 +57,7 @@ ADDR_KBRD:
 ##############################################################################
 # Code
 ##############################################################################
+
 	.text
 	.globl main
 
@@ -70,7 +71,118 @@ game_loop:
     # 2a. Check for collisions
 	# 2b. Update locations (paddle, ball)
 	# 3. Draw the screen
+	li $s7, 0x0000ff        # $s7 = blue
+	li $s6, 0x17161A	 # $s6 = dark grey
+	li $s5, 0x454545	 # $s5 = light grey
+		
+	lw $s0, ADDR_DSPL       # $t0 = base address for display
+	addi $s0, $s0, 1064	 # move to the location of board
+	addi $s1, $zero, 12     # number of col
+	addi $s2, $zero, 22	 # number of rows
+	
+#	addi $sp, $sp, -4
+#	sw $s0, ($sp)
+#	sw $s1, ($sp)
+#	addi $sp, $sp, -4
+#	sw $s3, ($sp)
+#	jal Draw_Row
+	
+	# draws the left edge of border
+	
+	addi $sp, $sp, -4
+	sw $s0, ($sp)
+	addi $sp, $sp, -4
+	sw $s2, ($sp)
+	addi $sp, $sp, -4
+	sw $s7, ($sp)
+	jal Draw_Col
+	
+	addi $s0, $s0,44
+	
+	# draws the right edge of border
+	
+	addi $sp, $sp, -4
+	sw $s0, ($sp)
+	addi $sp, $sp, -4
+	sw $s2, ($sp)
+	addi $sp, $sp, -4
+	sw $s7, ($sp)
+	jal Draw_Col
+	
+	# draws the bottom edge of border
+	
+	addi $s0, $s0, 2644
+	
+	addi $sp, $sp, -4
+	sw $s0, ($sp)
+	addi $sp, $sp, -4
+	sw $s1, ($sp)
+	addi $sp, $sp, -4
+	sw $s7, ($sp)
+	jal Draw_Row
+	
+	# draws checkerboard pattern
+	
+	lw $s0, ADDR_DSPL       # $s0 = base address for display
+	addi $s0, $s0, 1064
+	addi $s1, $zero, 10
+	addi $s2, $zero, 20
+	
+	addi $t2, $zero, 0
+	li $t4, 0
+	BG_Row: 
+		beq $t2, $s2, BG_End_Row
+		xori $t4, $t4, 1
+		addi $t2, $t2, 1
+		addi $s0, $s0, 128
+		addi $t0, $s0, 0
+		
+		
+		addi $t1, $zero, 0
+		BG_Col:
+			beq $t1, $s1, BG_Row
+			addi $t1, $t1, 1
+			addi $t0, $t0, 4
+			beqz $t4 Light_Gray_Square
+			xori $t4, $t4, 1
+			sw $s6, ($t0)
+			j BG_Col
+			Light_Gray_Square: sw $s5, ($t0)
+			xori $t4, $t4, 1
+			j BG_Col
+	BG_End_Row:
 	# 4. Sleep
 
     #5. Go back to 1
     b game_loop
+	Draw_Row:
+		# t3 = color, t1 = length, t0 = starting position
+		lw $t3, ($sp)
+		addi $sp, $sp, 4
+		lw $t1, ($sp)
+		addi $sp, $sp, 4
+		lw $t0, ($sp)
+		addi $sp, $sp, 4
+		
+		Loop_Row:
+		beqz $t1, End_Row 
+		sw $t3, ($t0)
+		addi $t0, $t0, 4
+		subi $t1, $t1, 1
+		j Loop_Row
+		End_Row: jr $ra
+	Draw_Col:
+		# t3 = color, t1 = length, t0 = starting position
+		lw $t3, ($sp)
+		addi $sp, $sp, 4
+		lw $t1, ($sp)
+		addi $sp, $sp, 4
+		lw $t0, ($sp)
+		addi $sp, $sp, 4
+		Loop_Col:
+		beqz $t1, End_Col
+		sw $t3, ($t0)
+		addi $t0, $t0, 128
+		subi $t1, $t1, 1
+		j Loop_Col
+		End_Col: jr $ra
